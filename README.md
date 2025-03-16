37845349842623f19ba95ad644dadc5840390bc1
spabas <bastian.spaeth@gmx.de>
2025-03-16 10:27:56 +0100

    lib update

--- README.md
+++ README.md
@@ -19,6 +19,10 @@ Get informations for libreo wallboxes
 	Placeholder for the next version (at the beginning of the line):
 	### **WORK IN PROGRESS**
 -->
+
+### **WORK IN PROGRESS**
+* (spabas) Lib update
+
 ### 1.4.4 (2025-03-15)
 * (spabas) Lib update
 
--- main.js
+++ main.js
@@ -9,7 +9,7 @@
 const utils = require("@iobroker/adapter-core");
 
 // Load your modules here, e.g.:
-const axios = require("axios");
+const axios = require("axios").default;
 const cheerio = require("cheerio");
 const { wrapper } = require("axios-cookiejar-support");
 const tough = require("tough-cookie");
